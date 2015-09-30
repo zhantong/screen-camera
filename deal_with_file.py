f3a73f11dd4d42596af65a2dfe0a71e8ac785c85
zhantong1994 <zhantong1994@163.com>
2015-09-30 11:25:12 +0800

    增加转换为二维码和从二维码转回为二进制字符串

--- deal_with_file.py
+++ deal_with_file.py
@@ -1,3 +1,5 @@
+from PIL import Image
+import re
 def compress(path):
 	with open(path,'rb') as f:
 		src=f.read()
@@ -18,7 +20,38 @@ def uncompress(code,path):
 	b = bytearray([int(code[x:x+8], 2) for x in range(0, len(code), 8)])
 	with open(path,'wb') as f:
 		f.write(b)
-
+def to_image(code):
+	img=Image.new('1',(20,20))
+	pixels=img.load()
+	length=len(code)
+	c=0
+	flag=1
+	for line in range(img.size[1]):
+		for row in range(img.size[0]):
+			if c<length:
+				pixels[row,line]=int(code[c])
+				c+=1
+			else:
+				pixels[row,line]=1
+				if flag:
+					pixels[row,line]=0
+					flag=0
+	img.save('text.bmp')
+def from_image(path):
+	img=Image.open(path)
+	pixels=img.load()
+	result=''
+	for line in range(img.size[1]):
+		for row in range(img.size[0]):
+			if pixels[row,line]==0:
+				result+='0'
+			else:
+				result+='1'
+	match=re.search(r'01+$',result)
+	return result[:match.start()]
 if __name__=='__main__':
-	path='第七讲.ppt'
-	uncompress(com(path),'text1')
+	path='text'
+	#uncompress(com(path),'text1')
+	print(com(path))
+	to_image(com(path))
+	print(from_image('text.bmp'))
